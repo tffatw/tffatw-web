ccc884dc7e1a08c4d85cc6bb0f16bb67c3fed01a
YochLin <speak14152@gmail.com>
2026-06-21 20:55:14 +0800

    feat: migrate to Cloudflare Workers with auto-deploy

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: { unoptimized: true },
 }
 
 module.exports = nextConfig
+
+import('@opennextjs/cloudflare').then(m => m.initOpenNextCloudflareForDev());
